00ea3137a71d7736d97b5a6819e52e01840de094
Ali Bas <alibas01@gmail.com>
2021-01-13 16:44:34 -0500

    solution without stretch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
 const wordSearch = (letters, word) => { 
+    const verticalArray = transpose(letters).map(ls => ls.join(''));
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
     }
+    for(let x of verticalArray) {
+        if (x.includes(word)) return true;
+    }
+    return false;
 }
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+    let output = [];
+    
+    for (const row in matrix[0]) { //0,1
+      let arr = [];
+      for (const col in matrix) { //0,1,2
+        arr.push(matrix[col][row]);
+      }
+      output.push(arr);
+    }
+  
+    return output;
+    
+  };
+
+  module.exports = wordSearch;
