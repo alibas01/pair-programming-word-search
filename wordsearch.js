5467a8f9782b8da2369501b2efda15b6fd6fee48
Ali Bas <alibas01@gmail.com>
2021-07-01 08:54:25 -0400

    better shape

--- wordsearch.js
+++ wordsearch.js
@@ -1,28 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const verticalArray = transpose(letters).map(ls => ls.join(''));
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true;
-    }
-    for(let x of verticalArray) {
-        if (x.includes(word)) return true;
-    }
-    return false;
-}
+const wordSearch = (letters, word) => {
+  const verticalArray = transpose(letters).map((ls) => ls.join(""));
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (let x of verticalArray) {
+    if (x.includes(word)) return true;
+  }
+  return false;
+};
 
 const transpose = function(matrix) {
-    let output = [];
-    
-    for (const row in matrix[0]) { //0,1
-      let arr = [];
-      for (const col in matrix) { //0,1,2
-        arr.push(matrix[col][row]);
-      }
-      output.push(arr);
-    }
-  
-    return output;
-    
-  };
+  let output = [];
 
-  module.exports = wordSearch;
+  for (const row in matrix[0]) {
+    //0,1
+    let arr = [];
+    for (const col in matrix) {
+      //0,1,2
+      arr.push(matrix[col][row]);
+    }
+    output.push(arr);
+  }
+
+  return output;
+};
+
+module.exports = wordSearch;
